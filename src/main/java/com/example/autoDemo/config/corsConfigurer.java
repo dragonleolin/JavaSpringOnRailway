9e39771ddaa1588b86c0cfaed20318218481e80a
luke <https://gitlab.com/luke.lin2com>
2025-07-18 17:11:16 +0800

    test

---
+++ src/main/java/com/example/autoDemo/config/corsConfigurer.java
@@ -0,0 +1,19 @@
+package com.example.autoDemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class corsConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://dragonleolin.github.io/Vue_StockDemo/")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
